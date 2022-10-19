7cadf6f9f58d24f2bd70782aede78eb0218931c9
SamoKopecky <skopecky@redhat.com>
2022-10-19 20:51:15 +0200

    feat: Create new error handler
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

--- plugins/cluster-status-backend/src/exceptions/ServerConfigurationError.ts
+++ plugins/cluster-status-backend/src/exceptions/ServerConfigurationError.ts
@@ -1,7 +1,11 @@
 import { ClusterStatusError } from "./ClusterStatusError";
 
 export class ServerConfigurationError extends ClusterStatusError {
+<<<<<<< HEAD
   constructor(msg: string, description: string) {
+=======
+  constructor(msg: string, description?: string) {
+>>>>>>> 66094c6 (feat: Create new error handler)
     super(msg, 'server-configuration-error', 500, description);
   }
 }
